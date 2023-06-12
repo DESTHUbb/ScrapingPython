1797ad002ed3303c249366932ba30b31907cd500
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-12 21:09:09 +0200

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -54,3 +54,5 @@ print(bold_tags)
 
 url = "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
 
+web_html_content = get_html_from_url(url)
+
