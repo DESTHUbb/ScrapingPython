e61b1359967fbfc916b34b8fbafcf6e39f3932a7
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-13 19:47:23 +0200

    Update web_scraping2.py

--- web_scraping2.py
+++ web_scraping2.py
@@ -27,3 +27,12 @@ for tag in soup.find_all("input", type="text"):
 with open("changed.html", "w") as file:
     file.write(str(soup))
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Improvements and updates made:
+1: # Reading the HTML file: In the original version, the open method was used to read the HTML file and then passed to the BeautifulSoup constructor. 
+   # In the improved version, open is used to read the entire contents of the file in a single text string, and this string is passed to the BeautifulSoup constructor.
+
+
+
+	
