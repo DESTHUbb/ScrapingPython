ec19bfd40f05715be3f9f89097774a0be0eb2dd3
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-15 17:02:05 +0200

    Update web_scraping4.py

--- Web 4/web_scraping4.py
+++ Web 4/web_scraping4.py
@@ -89,6 +89,7 @@ url = f"{base_url}?{'&'.join([f'{k}={v}' for k,v in params.items()])}"
   for page in range(1, pages + 1):
         url = f"{base_url}?{'&'.join([f'{k}={v}' for k,v in params.items()])}&page={page}"
         items_found.update(get_items(url, search_term))
+    sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
 
 
 
